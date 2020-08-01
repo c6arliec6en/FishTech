13ea3bdb0650a25eefe90ef8afac94db8b8b8c6f
Charlie <chen_610146@hotmail.com>
2020-08-01 12:20:44 +0800

    edit contact and css

--- main.js
+++ main.js
@@ -1,46 +1,35 @@
-//WARGAMES JOSHUA SIMULATOR 3000
-let step = 0
-let name = ''
-let phoneNumber = ''
-let comment = ''
-$('#contact').on('click', function () {
-  $('span').focus()
-})
-$('span').on('input', function () {
-  var v = $(this).text()
-  v = v.replace(/\s/g, '')
-  t = $(this)
-  l = $('li:last-of-type')
+const form = document.querySelector('#contact-form')
 
-  if (step == 0) {
-    name = v
-  } else if (step == 1) {
-    phoneNumber = v
-  } else if (step == 2) {
-    comment = v
-  }
-})
+//form information
+const input = document.querySelectorAll('.c-input')
+const text = document.querySelector('.c-text')
 
-$('span').on('keyup', (e) => {
-  if (e.keyCode == 13) {
-    if (step == 0 && name.length > 0) {
-      l.after(`<li>Hi ${name}！請留下真的找得到你的電話`)
-      t.text('')
-      step = 1
-    } else if (step == 1 && phoneNumber.length > 0) {
-      l.after(`<li>｡◕◡◕｡)ﾉ 非常感谢，有什麼疑惑或感興趣的項目嗎？歡迎告訴我們`)
-      t.text('')
-      step = 2
-    } else if (step == 2 && comment.length > 0) {
-      l.after(`<li>幫你打開信箱了，記得寄信給我們唷 ヾ(＠゜▽゜@)ノ`)
-      t.text('')
-      window.location.href = `mailto:michelle210181@gmail.com ?Subject=${name} 詢問網頁製作, 電話號碼 ${phoneNumber}&body=${comment}`
-    }
+form.addEventListener('submit', () => {
+  event.preventDefault()
+
+  const name = input[0].value
+  const email = input[1].value
+  const phone = input[2].value
+
+  if (!name) {
+    console.log('name cant be empty')
+  } else if (!email) {
+    console.log('Email cant be empty')
+  } else if (!text.value) {
+    console.log('info cant be empty')
   } else {
-    return
-  }
-})
 
-setInterval(function () {
-  $('.blink').toggleClass('blinker')
-}, 400)
+    var template_params = {
+      "name": name,
+      "email": email,
+      "phone": phone,
+      "info": text.value
+    }
+
+    var service_id = "default_service";
+    var template_id = "demand";
+    emailjs.send(service_id, template_id, template_params);
+
+  }
+
+})
